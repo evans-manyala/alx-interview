b5333f6884f3e0077a59e75528c1e2c6e2f78781
Evans Manyala <evans.manyala@gmail.com>
2024-07-08 01:17:27 +0300

    Making code executable

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,27 +10,19 @@ locked by a sequence of keys in other boxes. Can you open all the boxes?
 
 
 def canUnlockAll(boxes):
-    # Number of boxes
+    """ Determines if all the boxes can be opened. """
     n = len(boxes)
-    
-    # Set of opened boxes
     opened = set()
-    
-    # Queue for BFS, starting with the first box
     queue = [0]
-    
+
     while queue:
         box_index = queue.pop(0)
-        
-        # If the box is not already opened
+
         if box_index not in opened:
-            # Open the box
             opened.add(box_index)
-            
-            # Add all keys in this box to the queue
+
             for key in boxes[box_index]:
-                if key < n:  # Ensure the key is valid
+                if key < n:
                     queue.append(key)
-    
-    # Check if all boxes have been opened
+
     return len(opened) == n
