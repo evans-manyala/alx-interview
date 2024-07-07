88c7db934549861e02833fd7eb5cafd9880cb95d
Evans Manyala <evans.manyala@gmail.com>
2024-07-08 01:15:59 +0300

    Adding code logic documentation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,19 +10,27 @@ locked by a sequence of keys in other boxes. Can you open all the boxes?
 
 
 def canUnlockAll(boxes):
-    """ Determines if all the boxes can be opened. """
+    # Number of boxes
     n = len(boxes)
+    
+    # Set of opened boxes
     opened = set()
+    
+    # Queue for BFS, starting with the first box
     queue = [0]
-
+    
     while queue:
         box_index = queue.pop(0)
-
+        
+        # If the box is not already opened
         if box_index not in opened:
+            # Open the box
             opened.add(box_index)
-
+            
+            # Add all keys in this box to the queue
             for key in boxes[box_index]:
-                if key < n:
+                if key < n:  # Ensure the key is valid
                     queue.append(key)
-
+    
+    # Check if all boxes have been opened
     return len(opened) == n
