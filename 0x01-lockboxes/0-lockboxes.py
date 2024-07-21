f4392e29b1f449d51d70d576ec575e96db0e486d
Evans Manyala <evans.manyala@gmail.com>
2024-07-22 00:55:02 +0300

    Adding script error fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,16 +6,19 @@ of lists can be opened using keys stored in the lists
 
 
 def canUnlockAll(boxes):
-    """Determines if all the boxes can be opened."""
+    """ Determines if all the boxes can be opened. """
     n = len(boxes)
-    opened = [False] * n
-    opened[0] = True
-    keys = [0]
+    opened = set()
+    queue = [0]
 
-    while keys:
-        current_key = keys.pop()
-        for key in boxes[current_key]:
-            if key < n and not opened[key]:
-                opened[key] = True
-                keys.append(key)
-        return all(opened)
+    while queue:
+        box_index = queue.pop(0)
+
+        if box_index not in opened:
+            opened.add(box_index)
+
+            for key in boxes[box_index]:
+                if key < n and key not in opened:
+                    queue.append(key)
+
+    return all(opened)
