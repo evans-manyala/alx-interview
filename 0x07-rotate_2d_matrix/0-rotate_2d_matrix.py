1c00f56bd393999b9bc673f4a576c8015077239b
evans-manyala <evans.manyala@gmail.com>
2024-08-14 12:18:58 +0300

    Fixing transposeing errors

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,21 +3,23 @@
 Given an n x n 2D matrix, rotate it 90 degrees clockwise.
 
 """
+
+
 def rotate_2d_matrix(matrix):
-  """
-  Rotates a 2D matrix by 90 degrees clockwise in-place.
+    """
+    Rotates a 2D matrix by 90 degrees clockwise in-place.
 
-  Args:
-    matrix: A 2D list representing the matrix.
-  """
+    Args:
+      matrix: A 2D list representing the matrix.
+    """
 
-  n = len(matrix)
+    n = len(matrix)
 
-  # Transpose the matrix
-  for i in range(n):
-    for j in range(i, n):
-      matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-  # Reverse each row
-  for i in range(n):
-    matrix[i].reverse()
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
