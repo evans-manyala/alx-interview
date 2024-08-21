bdeca76bbc97136a243f9ddbdd46d2be4b0e2396
Evans Manyala <evans.manyala@gmail.com>
2024-08-21 22:28:57 +0300

    Fixing errors in script

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,11 +9,11 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('infinite')] * (total + 1)
+    dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    return dp[total] if dp[total] != float('infinite') else -1
+    return dp[total] if dp[total] != float('inf') else -1
