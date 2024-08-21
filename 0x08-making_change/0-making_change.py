c7626f2b685e26232f326653b2d3d1e655b7d15b
Evans Manyala <evans.manyala@gmail.com>
2024-08-21 22:26:30 +0300

    function to determine the fewest number of coins needed to meet a given amount total

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Function to solve a problem of determining the fewest number
+of coins needed to meet a given total.
+"""
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    dp = [float('infinite')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('infinite') else -1
