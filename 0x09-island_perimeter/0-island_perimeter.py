c3af50a89be41aecd36858b3413f1c6a3c0cc7e1
evans-manyala <evans.manyala@gmail.com>
2024-08-27 13:17:06 +0300

    Fixing errors in script

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,22 +5,30 @@ Function that returns the perimeter of the island described in grid:
 
 
 def island_perimeter(grid):
+    """Method Calculates the perimeter of a single island in a 2D grid.
+
+    Args:
+        grid: A 2D list of integers representing the grid.
+            - 0 represents water
+            - 1 represents land
+
+    Returns:
+        The perimeter of the island.
+    """
+
     perimeter = 0
-
-    rows = len(grid)
-    cols = len(grid[0])
-
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 1:
-
-                if r == 0 or grid[r - 1][c] == 0:
-                    perimeter += 1
-                if r == rows - 1 or grid[r + 1][c] == 0:
-                    perimeter += 1
-                if c == 0 or grid[r][c - 1] == 0:
-                    perimeter += 1
-                if c == cols - 1 or grid[r][c + 1] == 0:
-                    perimeter += 1
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                neighbors = 0
+                if i > 0 and grid[i - 1][j] == 1:
+                    neighbors += 1
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    neighbors += 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    neighbors += 1
+                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
+                    neighbors += 1
+                perimeter += 4 - neighbors
 
     return perimeter
