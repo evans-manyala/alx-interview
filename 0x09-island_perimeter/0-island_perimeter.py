6cf889eed5d27529029c0fbc1fd909ac0225fb25
evans-manyala <evans.manyala@gmail.com>
2024-08-27 12:57:21 +0300

    Initial Commit of a function that returns the perimeter of the island described in grid

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Function that returns the perimeter of the island described in grid:
+"""
+
+
+def island_perimeter(grid):
+    perimeter = 0
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                
+                if r == 0 or grid[r - 1][c] == 0:
+                    perimeter += 1
+                if r == rows - 1 or grid[r + 1][c] == 0:
+                    perimeter += 1
+                if c == 0 or grid[r][c - 1] == 0:
+                    perimeter += 1
+                if c == cols - 1 or grid[r][c + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
