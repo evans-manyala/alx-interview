aca41de2b7f342f6bb8b50668e0f281774da1a48
evans-manyala <evans.manyala@gmail.com>
2024-08-27 13:12:10 +0300

    Removing extra space

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,7 +13,7 @@ def island_perimeter(grid):
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-                
+
                 if r == 0 or grid[r - 1][c] == 0:
                     perimeter += 1
                 if r == rows - 1 or grid[r + 1][c] == 0:
